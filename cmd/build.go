b02eb6af1e797ed0c0477ad7a4cc276a811fa8d1
Daan Gerits <dgerits@korfinancial.com>
2023-07-12 09:07:48 +0200

    added a branch logic step and fixed some bugs

--- cmd/build.go
+++ cmd/build.go
@@ -6,17 +6,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var generateCmd = &cobra.Command{
-	Use:   "generate [path]",
+var buildCmd = &cobra.Command{
+	Use:   "build [path]",
 	Short: "Generate artifacts based on the given path",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		if err := generator.Generate(args[0]); err != nil {
-			logrus.Errorf("failed to generate artifact: %v", err)
+			logrus.Errorf("failed to build artifact: %v", err)
 		}
 	},
 }
 
 func init() {
-	ArtifactCmd.AddCommand(generateCmd)
+	ArtifactCmd.AddCommand(buildCmd)
 }
